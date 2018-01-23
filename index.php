09c9d989f1d8adfa8528610501465926195184ac
mkhozhylo <m.khozhylo@gmail.com>
2018-01-23 14:07:17 +0200

    new site for photograph

---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Photograph</title>
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href ="assets/css/default.css"/>
+	<link rel="stylesheet" type="text/css" href ="assets/css/main.css"/>
+</head>
+<body>
+    <div id="page" class="page">
+        <div class="header">
+            <div class="header-ct nw">
+                <div class="header-logo">
+                    <a href=""><img src="assets/img/img/logo.png"></a>
+                </div>
+                <div class="header-menu">
+                    <ul>
+                        <li><a>Обо мне</a></li>
+                        <li><a>Портфолио</a></li>
+                        <li><a>Услуги</a></li>
+                        <li><a>Цены</a></li>
+                        <li><a>Отзывы</a></li>
+                        <li><a>Блог</a></li>
+                        <li><a>Контакты</a></li>
+                    </ul>
+                </div>
+                <button class="header-btn"></button>
+            </div>
+        </div>
+        <div id="page-cont" class="page-cont page-hp">
+            <div class="hp-hello">
+                <div class="hello-cont nw">
+                    <p>email: alexhol@ua.fm</p>
+                    <p>tel: 066 898 1001</p>
+                    <p>097 922 17 26</p>
+                    <button class="hello-btn">закажите фотосессию прямо сейчас</button>
+                </div>
+                <button class="hello-next"></button>
+            </div>
+            <div class="hp-about">
+                <div class="about-cont nw">
+                    <div class="about-info">
+                        <h2>Добро пожаловать в сказочный мир моей фотографии</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea comm</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div class="about-foto">
+                        <img src="assets/img/img/fotoph 1.png">
+                    </div>
+                </div>
+            </div>
+            <div class="hp-job">
+                <div class="job-title">Мои последние работы</div>
+                <div class="job-foto">
+                    <div class="foto-item one">
+                        <a href="">
+                            <img src="assets/img/img/pic1.png">
+                        </a>
+                        <a href="">
+                            <img src="assets/img/img/pic2.png">
+                        </a>
+                    </div>
+                    <div class="foto-item two">
+                        <a href="">
+                            <img src="assets/img/img/pic3.png">
+                        </a>
+                    </div>
+                    <div class="foto-item three">
+                        <a href="">
+                            <img src="assets/img/img/pic4.png">
+                        </a>
+                    </div>
+                    <div class="foto-item three">
+                        <a href="">
+                            <img src="assets/img/img/pic5.png">
+                        </a>
+                    </div>
+                    <div class="foto-item one">
+                        <a href="">
+                            <img src="assets/img/img/pic6.png">
+                        </a>
+                        <a href="">
+                            <img src="assets/img/img/pic7.png">
+                        </a>
+                    </div>
+                    <div class="foto-item two">
+                        <a href="">
+                            <img src="assets/img/img/pic.png">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="hp-price">
+                <div class="price-cont nw">
+                    <div class="price-title">Цены на мои услуги</div>
+                    <div class="price-box">
+                        <div class="box-item">
+                            <h3>Пакет мини</h3>
+                            <h4>2000</h4>
+                            <div class="box-item-text">
+                                <p>ergergeg</p>
+                                <p>dfdfgg</p>
+                                <p>dfbhggmjhm</p>
+                                <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p>  
+                            </div>
+                            <button class="price-zakaz">заказать</button>
+                        </div>
+                        <div class="box-item">
+                            <h3>Пакет мини</h3>
+                            <h4>2000</h4>
+                            <div class="box-item-text">
+                                <p>ergergeg</p>
+                                <p>dfdfgg</p>
+                                <p>dfbhggmjhm</p>
+                                <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p> 
+                                <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p>
+                            </div>
+                            <button class="price-zakaz">заказать</button>
+                        </div>
+                        <div class="box-item">
+                            <h3>Пакет мини</h3>
+                            <h4>2000</h4>
+                           <div class="box-item-text">
+                                <p>ergergeg</p>
+                                <p>dfdfgg</p>
+                                <p>dfbhggmjhm</p>
+                                <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p> 
+                               <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p>
+                            </div>
+                            <button class="price-zakaz">заказать</button>
+                        </div>
+                        <div class="box-item">
+                            <h3>Пакет мини</h3>
+                            <h4>2000</h4>
+                           <div class="box-item-text">
+                                <p>ergergeg</p>
+                                <p>dfdfgg</p>
+                                <p>dfbhggmjhm</p>
+                                <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p>  
+                               <p>cbvbcvbcv</p>
+                                <p>cvbcvbcb</p>
+                            </div>
+                            <button class="price-zakaz">заказать</button>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="hp-feedback">
+                <div class="feedback-title">Отзывы</div>
+                <div class="feedback-cont">
+                    <div class="feedback nw">
+                        <div class="feedback-item">
+                            <div class="feedback-foto"><img src="assets/img/img/circle1.png" alt=""></div>
+                            <div class="feedback-text">
+                                <h4><i>Elen</i></h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            </div>
+                        </div>
+                        <div class="feedback-item">
+                            <div class="feedback-foto"><img src="assets/img/img/circle2.png" alt=""></div>
+                            <div class="feedback-text">
+                                <h4><i>Elen</i></h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            </div>
+                        </div>
+                        <div class="feedback-item">
+                            <div class="feedback-foto"><img src="assets/img/img/circle3.png" alt=""></div>
+                            <div class="feedback-text">
+                                <h4><i>Elen</i></h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            </div>
+                        </div>
+                        <div class="feedback-item">
+                            <div class="feedback-foto"><img src="assets/img/img/circle4.png" alt=""></div>
+                            <div class="feedback-text">
+                                <h4><i>Elen</i></h4>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="hp-contact">
+                <div class="contact-title">контакты</div>
+                <div class="contact">
+                    <div class="contact-cont nw">
+                        <div class="contact-info">
+                            <h3>John Doe</h3>
+                            <p>email: john.doe@email.com</p>
+                            <p>тел: 066 898 10 01 <i>(MTC)</i></p>
+                            <p>097 922 17 26 <i>(KS)</i></p>
+                            <div class="contact-cos">
+                                <ul>
+                                    <li>
+                                        <a href=""><img src="assets/img/icon/soc-social.png" alt=""></a>
+                                    </li>
+                                    <li>
+                                        <a href=""><img src="assets/img/icon/soc-behance.png" alt=""></a>
+                                    </li>
+                                    <li>
+                                        <a href=""><img src="assets/img/icon/soc-facebook-logo.png" alt=""></a>
+                                    </li>
+                                    <li>
+                                        <a href=""><img src="assets/img/icon/soc-twitter.png" alt=""></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="contact-form">
+                            <input type="text" class="input" id="name" name="name" placeholder="Ваше имя">
+                            <input type="email" class="input" id="email" name="email" placeholder="Ваше email">
+                            <textarea type="text" class="input textarea" id="message" name="message" placeholder="Ваше сообщение"></textarea>
+                            <input type="hidden" name="control" value="contactForm">
+                            <input type="button" class="button sendMessage" value="Отправить">
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+	
+</body>
+</html>
